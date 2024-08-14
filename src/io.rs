a08029f4702a49c9090256643969ee37a3dcc637
teeaychem <bsparkes@stanford.edu>
2024-08-14 14:45:29 +0100

    some basic dimacs parsing

---
+++ src/io.rs
@@ -0,0 +1,44 @@
+use crate::ideas::*;
+
+impl Clause {
+    pub fn from_dimacs(string: &str) -> Result<Clause, ClauseError> {
+        let literals = string.split_whitespace();
+        let mut the_clause = Clause::new();
+        for literal in literals {
+            let _ = match Literal::from_string(literal) {
+                Ok(made) => the_clause.add_literal(made),
+                Err(e) => {
+                    return Err(ClauseError::Literal(e));
+                }
+            };
+        }
+        Ok(the_clause)
+    }
+}
+
+impl Cnf {
+    pub fn from_dimacs(string: &str) -> Result<Cnf, CnfError> {
+        let mut cnf = Cnf::new();
+
+        let mut from = 0;
+        let mut to = 0;
+        while let Some(ch) = string.chars().nth(to) {
+            if ch == '0' {
+                match Clause::from_dimacs(&string[from..to]) {
+                    Ok(clause) => cnf.add_clause(clause),
+                    Err(e) => {
+                        return Err(CnfError::Clause(e));
+                    }
+                };
+                from = to + 1;
+            }
+            to += 1;
+        }
+
+        if !&string[from..].trim().is_empty() {
+            Err(CnfError::UnexpectedInformation)
+        } else {
+            Ok(cnf)
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,132 +1,28 @@
-use std::{cmp::Ordering, collections::BTreeSet};
+use clap::Parser;
+use std::fs;
+mod ideas;
+mod io;
+
+use crate::ideas::*;
+
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// file to parse
+    #[arg(short, long)]
+    file: String,
+}
+
 
 fn main() {
     println!("Hello, world!");
-}
-
-type Variable = usize;
-
-#[derive(Clone, Copy)]
-struct Literal {
-    variable: Variable,
-    polarity: bool,
-}
-
-impl PartialOrd for Literal {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Literal {
-    fn cmp(&self, other: &Self) -> Ordering {
-        if self.variable == other.variable {
-            if self.polarity == other.polarity {
-                Ordering::Equal
-            } else if self.polarity {
-                Ordering::Less
-            } else {
-                Ordering::Greater
-            }
-        } else {
-            self.variable.cmp(&other.variable)
+    let args = Args::parse();
+    dbg!(&args);
+    if let Ok(contents) = fs::read_to_string(args.file) {
+        if let Ok(the_cnf) = Cnf::from_dimacs(&contents) {
+            dbg!(&the_cnf);
         }
     }
-}
-
-impl PartialEq for Literal {
-    fn eq(&self, other: &Self) -> bool {
-        self.variable == other.variable && self.polarity == other.polarity
-    }
-}
-
-impl Eq for Literal {}
-
-type ClauseId = usize;
-
-struct Clause {
-    id: ClauseId,
-    literals: Vec<Literal>,
-}
-
-impl Clause {
-    ///
-    pub fn get_unit_on(&self, assignment: &Assignment) -> Option<(Literal, ClauseId)> {
-        let mut unit = None;
-        for literal in &self.literals {
-            if let Some(assignment) = assignment.get(literal.variable) {
-                match assignment {
-                    Some(true) => break,     // as the clause does not provide any new information
-                    Some(false) => continue, // some other literal must be true
-                    None => {
-                        // if no assignment, then literal must be true…
-                        match unit {
-                            Some(_) => {
-                                // æbut if there was already a literal, it's not implied
-                                unit = None;
-                                break;
-                            }
-                            None => unit = Some((*literal, self.id)), // still, if everything so far is false, this literal must be true, for now…
-                        }
-                    }
-                }
-            }
-        }
-        unit
-    }
-}
-
-struct Cnf {
-    variables: usize,
-    clauses: Vec<Clause>,
-}
-
-/// how a literal was added to an assignment
-#[derive(PartialEq, Eq)]
-enum Source {
-    Choice,
-    Deduction,
-}
-
-type Assignment = Vec<Option<bool>>;
-
-/// a partial assignment with some history
-// the assignment
-struct Trail {
-    assignment: Assignment,
-    history: Vec<(Literal, Source)>,
-}
-
-impl Trail {
-    /// work back through steps of the trail, discarding the trail, and relaxing the assignment
-    // here, some deduced literals may still hold, but for now the trail does not record multiple paths to a deduction
-    pub fn track_back(&mut self, steps: usize) {
-        for _step in 0..steps {
-            if let Some((literal, _)) = self.history.pop() {
-                self.assignment[literal.variable] = None
-            };
-        }
-    }
-}
-
-impl Trail {
-    pub fn backtrack_to_choice(&mut self) -> bool {
-        let mut back_point = self.history.len() - 1;
-        let mut back_steps = 0;
-        while let Some((_, Source::Deduction)) = self.history.get(back_point) {
-            back_point -= 1;
-            back_steps += 1;
-        }
-        if back_steps != 0 {
-            self.track_back(back_steps);
-            true
-        } else {
-            false
-        }
-    }
-}
-
-struct Solve {
-    cnf: Cnf,
-    trail: Trail,
 }
