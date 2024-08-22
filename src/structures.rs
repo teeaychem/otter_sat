72e1b91d19d3725a248e67045030a4082f0e057e
teeaychem <bsparkes@stanford.edu>
2024-08-22 10:35:20 +0100

    implication graphs to separate file

--- src/structures.rs
+++ src/structures.rs
@@ -3,9 +3,11 @@ pub mod clause;
 pub mod literal;
 pub mod solve;
 pub mod valuation;
+pub mod implication_graph;
 
 pub use crate::structures::assignment::{Assignment, AssignmentError, Level};
 pub use crate::structures::clause::{Clause, ClauseError, ClauseId};
 pub use crate::structures::literal::{Literal, LiteralError, LiteralSource, Variable, VariableId};
 pub use crate::structures::solve::{Solve, SolveError};
 pub use crate::structures::valuation::{Valuation, ValuationVec};
+pub use crate::structures::implication_graph::{EdgeId, ImplicationGraph};
