ac2b1e11c7a02ed38133184ea5ba4243d0acd7c9
teeaychem <bsparkes@stanford.edu>
2024-08-25 22:25:01 +0100

    some gnarly dimacs processing

--- src/io.rs
+++ src/io.rs
@@ -1,3 +1,5 @@
+use std::char;
+
 use crate::structures::*;
 
 enum IOError {
@@ -11,10 +13,57 @@ impl Solve {
 
         let mut from = 0;
         let mut to = 0;
+        let mut skip = false;
         while let Some(ch) = string.chars().nth(to) {
             if ch == '0' {
-                the_solve.add_clause(&string[from..to])?;
+                if !skip {
+                    the_solve.add_clause(&string[from..to])?;
+                }
                 from = to + 1;
+            } else if ch == 'c' {
+                skip = true;
+            } else if ch == 0xA as char {
+                // newline check
+                from += 1;
+                skip = false;
+            } else if ch == 'p' {
+                loop {
+                    to += 1;
+                    if let Some(other_ch) = string.chars().nth(to) {
+                        if other_ch == 0xA as char {
+                            break;
+                        }
+                    } else {
+                        return Err(SolveError::ParseFailure);
+                    }
+                }
+                let the_preface = &string[from..to];
+                let mut preface_parts = the_preface.split_whitespace();
+                preface_parts.next(); // skip 'p'
+                if to - from < 7 {
+                    return Err(SolveError::PrefaceLength);
+                } else if Some("cnf") != preface_parts.next() {
+                    return Err(SolveError::PrefaceFormat);
+                }
+                let variables = match preface_parts.next() {
+                    Some(count) => match count.parse::<usize>() {
+                        Ok(count_number) => count_number,
+                        Err(_) => return Err(SolveError::ParseFailure),
+                    },
+                    None => return Err(SolveError::ParseFailure),
+                };
+                let clauses = match preface_parts.next() {
+                    Some(count) => match count.parse::<usize>() {
+                        Ok(count_number) => count_number,
+                        Err(_) => return Err(SolveError::ParseFailure),
+                    },
+                    None => return Err(SolveError::ParseFailure),
+                };
+                from = to
+            }
+
+            if skip {
+                from += 1
             }
             to += 1;
         }
