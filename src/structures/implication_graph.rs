fab4f971e65de67c0ffc3f66d97cd2c55f26a89d
teeaychem <ben@sparkes.uk.net>
2024-08-27 15:41:38 +0100

    a few things all together
    the main solve loop now uses inconsistent/complete valuations to determine UN/SAT.
    to help do this unit propagation is revised to be derived from finding all the unset literals from the clauses given a valuation
    at the moment there's no real use for keeping al the possible choices, but these might be useful for heuristics
    
    the implication graph is also revised a little, and is created for a formula, rather than a level
    roughly, the idea is that a formula is known to be constant and so references can be passed around so long as there are sufficient lifetime guarantees
    to keep things simple for the moment, the guarantee is made by setting formulas to have static lifetime

--- src/structures/implication_graph.rs
+++ src/structures/implication_graph.rs
@@ -1,23 +1,21 @@
-use crate::structures::{ClauseId, Literal, Solve, Valuation, VariableId};
+use crate::structures::{ClauseId, Formula, Level, Literal, Solve, Valuation, VariableId};
 use std::collections::BTreeSet;
 
 // Implication graph
+
+#[derive(Clone, Debug)]
+pub struct ImpGraph {
+    formula: &'static Formula,
+    edges: Vec<ImpGraphEdge>,
+    nodes: Vec<ImpGraphNode>,
+    implication_paths: ImpGraphPaths,
+}
 
 #[derive(Clone, Debug)]
 pub struct ImpGraphNode {
     v_id: VariableId,
     backward_edges: Option<Vec<EdgeId>>,
     forward_edges: Option<Vec<EdgeId>>,
-}
-
-impl ImpGraphNode {
-    pub fn new(v_id: VariableId) -> Self {
-        ImpGraphNode {
-            v_id,
-            backward_edges: None,
-            forward_edges: None,
-        }
-    }
 }
 
 pub type EdgeId = usize;
@@ -27,16 +25,6 @@ pub struct ImpGraphEdge {
     from: VariableId,
     to: VariableId,
     clause_id: ClauseId,
-}
-
-impl ImpGraphEdge {
-    pub fn new(from: VariableId, clause_id: ClauseId, to: VariableId) -> Self {
-        ImpGraphEdge {
-            from,
-            clause_id,
-            to,
-        }
-    }
 }
 
 #[derive(Clone, Debug)]
@@ -64,81 +52,23 @@ impl ImpGraphPaths {
     }
 }
 
-#[derive(Clone, Debug)]
-pub struct ImpGraph {
-    pub units: Vec<(ClauseId, Literal)>,
-    edges: Vec<ImpGraphEdge>,
-    valuation_size: usize,
-    nodes: Vec<ImpGraphNode>,
-    implication_paths: ImpGraphPaths,
-}
-
 impl ImpGraph {
-    pub fn new(solve: &Solve) -> Self {
+    pub fn for_formula(formula: &'static Formula) -> ImpGraph {
         ImpGraph {
-            valuation_size: solve.valuation.len(),
-            units: vec![],
-            implication_paths: ImpGraphPaths::new_empty(),
+            formula,
             edges: vec![],
-            nodes: (0..solve.valuation.len())
-                .map(|i| ImpGraphNode::new(i as u32))
-                .collect::<Vec<_>>(),
-        }
-    }
-
-    pub fn for_level(solve: &Solve, level: usize) -> ImpGraph {
-        let valuation = &solve.valuation_at_level(level);
-        let the_units = solve.find_all_units_on(valuation, &mut BTreeSet::new());
-        let units: Vec<Literal> = the_units
-            .iter()
-            .map(|(_clause, literal)| literal)
-            .cloned()
-            .collect();
-
-        let relevant_ids = units
-            .iter()
-            .chain(solve.levels[level].literals().iter())
-            .map(|l| l.v_id)
-            .collect::<BTreeSet<_>>();
-
-        let mut relevant_edges: Vec<ImpGraphEdge> = vec![];
-        for (clause_id, to_literal) in &the_units {
-            for from_literal in &solve.formula.borrow_clause_by_id(*clause_id).literals {
-                if relevant_ids.contains(&from_literal.v_id) && from_literal != to_literal {
-                    relevant_edges.push(ImpGraphEdge::new(
-                        from_literal.v_id,
-                        *clause_id,
-                        to_literal.v_id,
-                    ));
-                }
-            }
-        }
-        // let edges = the_units
-        //     .iter()
-        //     .flat_map(|(clause_id, to_literal)| {
-        //         solve.clauses[*clause_id]
-        //             .literals
-        //             .iter()
-        //             .filter(|&l| l != to_literal)
-        //             .map(|from_literal| (from_literal.v_id, *clause_id, to_literal.v_id))
-        //             .collect::<Vec<_>>()
-        //     })
-        //     .collect::<Vec<_>>();
-        let mut the_graph = ImpGraph {
-            valuation_size: valuation.size(),
-            units: the_units,
-            edges: relevant_edges,
             implication_paths: ImpGraphPaths::new_empty(),
-            nodes: (0..valuation.size())
+            nodes: (0..formula.var_count())
                 .map(|i| ImpGraphNode::new(i as u32))
                 .collect::<Vec<_>>(),
-        };
-        the_graph.annotate_node_edges(true, true);
-
-        the_graph
+        }
     }
 
-    fn annotate_node_edges(&mut self, forwards: bool, backwards: bool) {
+    pub fn add_edge(&mut self, edge: ImpGraphEdge) {
+        self.edges.push(edge)
+    }
+
+    pub fn annotate_node_edges(&mut self, forwards: bool, backwards: bool) {
         for (edge_id, edge) in self.edges.iter().enumerate() {
             if forwards {
                 if let Some(node) = self.nodes.get_mut(edge.from as usize) {
@@ -169,12 +99,11 @@ impl ImpGraph {
     /* a dfs over the graph where each terminal node is given a unique number and any intermediate node has all the numbers of it's associated terminal nodes
     only terminal nodes are considered, as if non terminal then the literal was unit from the decision and so must be passed through anyway
 
-
     could be improved by ignoring edges which differ only by clause id
     */
     pub fn trace_implication_paths(&mut self, from: Vec<Literal>) {
         let mut path_obj = ImpGraphPaths::new_empty_of_size(
-            self.valuation_size,
+            self.formula.var_count(),
             from.iter()
                 .map(|l| l.v_id)
                 .filter(|&v_id| self.nodes[v_id as usize].forward_edges.is_none())
@@ -213,5 +142,29 @@ impl ImpGraph {
             helper(self, v as usize, path_obj.count, &mut path_obj);
         }
         self.implication_paths = path_obj
+    }
+
+    pub fn generate_details(&mut self) {
+        self.annotate_node_edges(true, true);
+    }
+}
+
+impl ImpGraphNode {
+    pub fn new(v_id: VariableId) -> Self {
+        ImpGraphNode {
+            v_id,
+            backward_edges: None,
+            forward_edges: None,
+        }
+    }
+}
+
+impl ImpGraphEdge {
+    pub fn new(from: VariableId, clause_id: ClauseId, to: VariableId) -> Self {
+        ImpGraphEdge {
+            from,
+            clause_id,
+            to,
+        }
     }
 }
