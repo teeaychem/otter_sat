4ea3ef34a8957020425dd2baac19ac9d579c7a7b
teeaychem <bsparkes@stanford.edu>
2024-09-18 11:56:22 +0100

    split stored clause details from clause details

--- src/structures.rs
+++ src/structures.rs
@@ -4,12 +4,11 @@ pub mod implication_graph;
 pub mod level;
 pub mod literal;
 pub mod solve;
+pub mod stored_clause;
 pub mod valuation;
 pub mod variable;
 
-pub use crate::structures::clause::{
-    binary_resolution, Clause, ClauseId, ClauseSource, ClauseVec, StoredClause,
-};
+pub use crate::structures::clause::{binary_resolution, Clause, ClauseId, ClauseVec};
 pub use crate::structures::formula::Formula;
 pub use crate::structures::implication_graph::{
     ImplicationEdge, ImplicationGraph, ImplicationSource,
@@ -17,5 +16,6 @@ pub use crate::structures::implication_graph::{
 pub use crate::structures::level::{Level, LevelIndex};
 pub use crate::structures::literal::{Literal, LiteralError, LiteralSource};
 pub use crate::structures::solve::{Solve, SolveError, SolveOk, SolveStatus};
+pub use crate::structures::stored_clause::{ClauseSource, StoredClause};
 pub use crate::structures::valuation::{Valuation, ValuationError, ValuationOk, ValuationVec};
-pub use crate::structures::variable::{Variable,VariableId};
+pub use crate::structures::variable::{Variable, VariableId};
