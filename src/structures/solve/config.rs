f33f08ffcea6a93a8bcaad6192b2529a70f592b0
teeaychem <bsparkes@stanford.edu>
2024-10-12 23:52:03 +0100

    improved config, patched propagation loop
    for the loop, the change to a function call made the break on conflict ineffective, this has been restored
    (or maybe it was broken before, either way both the clause loop and the q are broken from)
    this, naturally, helps with performance
    
    some effort has been made to improve the config
    clap has some nice features to help with this, and i've tried to make use of these
    a dependency on serde is a consequence, but that was present for logginig in any case
    
    i think the only addition is the option to randomly choose the polarity of a choice, which brings reliance on rand
    this can be configured by specifying a probability for the polarity to be `true`
    
    oh, and most of the functions used to split up the solve have been restated as methods
    this helps considerably with varying what is and isn't needed

--- src/structures/solve/config.rs
+++ src/structures/solve/config.rs
@@ -1,41 +1,52 @@
+use crate::structures::solve::Solve;
+use serde::Serialize;
+
 pub static ACTIVITY_CONFLICT: f32 = 1.0;
 pub static DECAY_FACTOR: f32 = 0.95;
 pub static DECAY_FREQUENCY: usize = 1;
-pub static mut VSIDS_VARIANT: VSIDS = VSIDS::M;
-pub static mut LUBY_CONSTANT: usize = 1024;
 
 // Configuration variables
-pub static mut GLUE_STRENGTH: usize = 2;
-pub static mut SHOW_STATS: bool = false;
-pub static mut SHOW_CORE: bool = false;
-pub static mut SHOW_VALUATION: bool = false;
 pub static mut EXPLORATION_PRIORITY: ExplorationPriority = ExplorationPriority::Default;
-pub static mut STOPPING_CRITERIA: StoppingCriteria = StoppingCriteria::FirstAssertingUIP;
-pub static mut RESTARTS_ALLOWED: bool = true;
+pub static mut GLUE_STRENGTH: usize = 2;
 pub static mut HOBSON_CHOICES: bool = false;
-pub static mut TIME_LIMIT: Option<std::time::Duration> = None;
-
+pub static mut LUBY_CONSTANT: usize = 512;
+pub static mut POLARITY_LEAN: f64 = 0.5;
 pub static mut REDUCTION_ALLOWED: bool = false;
+pub static mut RESTARTS_ALLOWED: bool = true;
+pub static mut SHOW_CORE: bool = false;
+pub static mut SHOW_STATS: bool = false;
+pub static mut SHOW_VALUATION: bool = false;
+pub static mut STOPPING_CRITERIA: StoppingCriteria = StoppingCriteria::FirstUIP;
+pub static mut TIME_LIMIT: Option<std::time::Duration> = None;
+pub static mut VSIDS_VARIANT: VSIDS = VSIDS::MiniSAT;
 
-use crate::structures::solve::Solve;
-
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Default, Serialize, clap::ValueEnum)]
+#[serde(rename_all = "kebab-case")]
 pub enum StoppingCriteria {
-    FirstAssertingUIP,
+    #[default]
+    /// Resolve until the first unique implication point
+    FirstUIP,
+    /// Resolve on each clause used to derive the conflict
     None,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Default, Serialize, clap::ValueEnum)]
+#[serde(rename_all = "kebab-case")]
 #[allow(clippy::upper_case_acronyms)]
 pub enum VSIDS {
-    M,
-    C,
+    #[default]
+    /// Bump the activity of all variables in the a learnt clause
+    MiniSAT,
+    /// Bump the activity involved when using resolution to learn a clause
+    Chaff,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, Default, Serialize, clap::ValueEnum)]
+#[serde(rename_all = "kebab-case")]
 pub enum ExplorationPriority {
     Conflict,
     Implication,
+    #[default]
     Default,
 }
 
@@ -56,5 +67,14 @@ fn luby(i: usize) -> usize {
             return luby(i - (1 << (k - 1)) + 1);
         }
         k += 1
+    }
+}
+
+impl std::fmt::Display for VSIDS {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Self::MiniSAT => write!(f, "MiniSAT"),
+            Self::Chaff => write!(f, "Chaff"),
+        }
     }
 }
