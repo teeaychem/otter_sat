9cd653f292f55aaff4fdc8d2f0f073c526b9d694
teeaychem <bsparkes@stanford.edu>
2024-10-15 11:05:54 +0100

    resolution buffer second pass, and some minor things
    the buffer from the previous commit has been tidied a little and various parts of conflict analysis have been implemented as methods on the buffer
    this helps simplify conflict analysis, so much so that the analysis is now refactored to a single function
    
    with this, ordering of clauses is no longer enforced, and so stored clauses no longer store an additional copy of the clause which preserves order
    and, functions for resolution, finding, etc have been removed

--- src/structures/solve/analysis.rs
+++ src/structures/solve/analysis.rs
@@ -1,235 +1,105 @@
 use crate::structures::{
     clause::{
         stored::{Source as ClauseSource, StoredClause},
-        Clause, ClauseVec,
+        Clause,
     },
     literal::{Literal, Source as LiteralSource},
-    solve::{config, retreive_unsafe, ClauseKey, Solve, Status},
-    valuation::Valuation,
+    solve::{
+        config, resolution_buffer::ResolutionBuffer, retreive_unsafe, ClauseKey, Solve, Status,
+    },
     variable::Variable,
 };
 
-use std::collections::VecDeque;
-
-#[derive(Debug, Clone, Copy, PartialEq)]
-enum RSItem {
-    Value(Option<bool>),
-    NoneLiteral(Literal),
-    ConflictLiteral(Literal),
-}
-
-#[derive(Debug)]
-struct ResolutionBuffer {
-    missing: usize,
-    asserts: Option<Literal>,
-    buffer: Vec<RSItem>,
-}
-
-impl ResolutionBuffer {
-    fn from_valuation(valuation: &impl Valuation) -> Self {
-        ResolutionBuffer {
-            missing: 0,
-            asserts: None,
-            buffer: valuation
-                .slice()
-                .iter()
-                .map(|value| RSItem::Value(*value))
-                .collect(),
-        }
-    }
-
-    fn eat_clause(&mut self, clause: &impl Clause) {
-        for literal in clause.literal_slice() {
-            match self.buffer.get(literal.index()).expect("wuh") {
-                RSItem::ConflictLiteral(_) | RSItem::NoneLiteral(_) => {}
-                RSItem::Value(maybe) => match maybe {
-                    None => {
-                        self.buffer[literal.index()] = {
-                            self.missing += 1;
-                            RSItem::NoneLiteral(*literal)
-                        }
-                    }
-                    Some(value) if *value == literal.polarity() => {
-                        panic!("huh")
-                    }
-                    Some(_) => self.buffer[literal.index()] = RSItem::ConflictLiteral(*literal),
-                },
-            }
-        }
-    }
-
-    fn resolve_clause(&mut self, clause: &impl Clause, using: Literal) -> bool {
-        if self.buffer[using.index()] == RSItem::NoneLiteral(using.negate()) {
-            for literal in clause.literal_slice() {
-                match self.buffer.get(literal.index()).expect("wuh") {
-                    RSItem::ConflictLiteral(_) | RSItem::NoneLiteral(_) => {}
-                    RSItem::Value(maybe) => match maybe {
-                        None => {
-                            self.missing += 1;
-                            self.asserts = Some(*literal);
-                            self.buffer[literal.index()] = RSItem::NoneLiteral(*literal)
-                        }
-                        Some(value) if *value == literal.polarity() => {
-                            panic!("huh")
-                        }
-                        Some(_) => self.buffer[literal.index()] = RSItem::ConflictLiteral(*literal),
-                    },
-                }
-            }
-            self.missing -= 1;
-            self.buffer[using.index()] = RSItem::Value(Some(false));
-            true
-        } else {
-            false
-        }
-    }
-
-    fn to_clause(&self) -> (Option<Literal>, Vec<Literal>) {
-        let mut the_clause = Vec::with_capacity(self.buffer.len());
-        let mut conflict_literal = None;
-        for item in &self.buffer {
-            match item {
-                RSItem::Value(_) => {}
-                RSItem::ConflictLiteral(literal) => the_clause.push(*literal),
-                RSItem::NoneLiteral(literal) => {
-                    conflict_literal = Some(*literal);
-                    the_clause.push(*literal)
-                }
-            }
-        }
-        (conflict_literal, the_clause)
-    }
-}
+use std::{collections::VecDeque, ops::Deref};
 
 impl Solve {
-    pub fn attempt_fix(&mut self, clause_key: ClauseKey) -> Status {
-        let conflict_clause =
-            retreive_unsafe(&self.formula_clauses, &self.learnt_clauses, clause_key);
-
-        log::trace!("Fix on clause {conflict_clause} @ {}", self.level().index());
+    pub fn conflict_analysis(&mut self, clause_key: ClauseKey) -> Status {
+        log::trace!("Fix @ {}", self.level().index());
         if self.level().index() == 0 {
             return Status::NoSolution;
         }
+        let conflict_clause =
+            retreive_unsafe(&self.formula_clauses, &self.learnt_clauses, clause_key);
+        log::trace!("Clause {conflict_clause}");
 
-        let mut previous_level_val = self.valuation.clone();
-        for literal in self.level().literals() {
-            previous_level_val[literal.index()] = None;
-        }
-        if let Some(asserted) = conflict_clause.asserts(&previous_level_val) {
+        let mut resolution_buffer = ResolutionBuffer::from_valuation(&self.valuation);
+        resolution_buffer.clear_literals(self.level().literals());
+        resolution_buffer.eat_clause(&conflict_clause.deref());
+
+        if let Some(asserted) = resolution_buffer.asserts() {
+            // check to see if missed
             let missed_level = backjump_level(&self.variables, conflict_clause.literal_slice());
             self.backjump(missed_level);
             self.literal_update(asserted, &LiteralSource::StoredClause(clause_key));
             self.consequence_q.push_back(asserted);
+
             Status::MissedImplication
         } else {
-            let (asserting_clause, clause_source, assertion) =
-                self.conflict_analysis(conflict_clause);
+            // resolve
+            resolution_buffer.resolve_with(
+                self.level().observations.iter().rev(),
+                &self.formula_clauses,
+                &self.learnt_clauses,
+            );
+            // see if resolution can be strengthened
+            resolution_buffer.strengthen_given(
+                self.levels[0]
+                    .observations
+                    .iter()
+                    .map(|(_, literal)| *literal),
+            );
 
-            let source = match asserting_clause.len() {
+            let (asserted_literal, mut resolved_clause) = resolution_buffer.to_assertion_clause();
+            if let Some(assertion) = asserted_literal {
+                resolved_clause.push(assertion);
+            }
+
+            self.apply_VSIDS(&resolved_clause, &resolution_buffer);
+
+            let source = match resolved_clause.len() {
                 1 => {
                     self.backjump(0);
-                    match clause_source {
-                        ClauseSource::Resolution(resolution_vector) => {
-                            LiteralSource::Resolution(resolution_vector)
-                        }
-                        ClauseSource::Formula => panic!("Analysis without resolution"),
-                    }
+                    LiteralSource::Resolution(resolution_buffer.trail().to_vec())
                 }
                 _ => {
-                    self.backjump(backjump_level(
-                        &self.variables,
-                        asserting_clause.literal_slice(),
-                    ));
+                    let backjump_level =
+                        backjump_level(&self.variables, resolved_clause.literal_slice());
+                    self.backjump(backjump_level);
+                    let clause_key = self.store_clause(
+                        resolved_clause,
+                        ClauseSource::Resolution(resolution_buffer.trail().to_vec()),
+                    );
 
-                    LiteralSource::StoredClause(self.store_clause(asserting_clause, clause_source))
+                    LiteralSource::StoredClause(clause_key)
                 }
             };
+            let assertion = asserted_literal.expect("w");
             self.literal_update(assertion, &source);
             self.consequence_q.push_back(assertion);
             Status::AssertingClause
         }
     }
 
-    /// Simple analysis performs resolution on any clause used to obtain a conflict literal at the current decision
-    pub fn conflict_analysis(
-        &self,
-        conflict_clause: &StoredClause,
-    ) -> (ClauseVec, ClauseSource, Literal) {
-        let mut resolved_clause = conflict_clause.clause_clone();
-        let mut resolution_trail = vec![];
-
-        let mut previous_level_val = self.valuation.clone();
-        for literal in self.level().literals() {
-            previous_level_val[literal.index()] = None;
-        }
-
-        let mut resolution_buffer = ResolutionBuffer::from_valuation(&previous_level_val);
-        resolution_buffer.eat_clause(&resolved_clause);
-
-        let mut asserted_literal = None;
-
-        let mut used_variables = vec![false; self.variables.len()];
-
-        'resolution_loop: for (src, literal) in self.level().observations.iter().rev() {
-            if let LiteralSource::StoredClause(clause_key) = src {
-                let stored_source_clause =
-                    retreive_unsafe(&self.formula_clauses, &self.learnt_clauses, *clause_key);
-
-                if resolution_buffer.resolve_clause(stored_source_clause, *literal) {
-                    resolution_trail.push(*clause_key);
-                }
-
-                for involved_literal in stored_source_clause.literal_slice() {
-                    used_variables[involved_literal.index()] = true;
-                }
-
-                if resolution_buffer.missing == 1 {
-                    (asserted_literal, resolved_clause) = resolution_buffer.to_clause();
-
-                    match unsafe { config::STOPPING_CRITERIA } {
-                        config::StoppingCriteria::FirstUIP => break 'resolution_loop,
-                        config::StoppingCriteria::None => {}
-                    }
-                };
-            }
-        }
-
-        /*
-        If some literals are known then their negation can be safely removed from the learnt clause.
-        Though, this isn't a particular effective method… really, iteration should be over the clause
-         */
-        for (_, other_literal) in &self.levels[0].observations {
-            if let Some(x) = resolved_clause.literal_position(other_literal.negate()) {
-                resolved_clause.remove(x);
-            }
-        }
-
+    #[allow(non_snake_case)]
+    fn apply_VSIDS(&self, clause: &impl Clause, buffer: &ResolutionBuffer) {
         unsafe {
             match config::VSIDS_VARIANT {
                 config::VSIDS::Chaff => {
-                    for literal in resolved_clause.literal_slice() {
+                    for literal in clause.literal_slice() {
                         self.variables
                             .get_unchecked(literal.index())
                             .add_activity(config::ACTIVITY_CONFLICT);
                     }
                 }
                 config::VSIDS::MiniSAT => {
-                    for (index, used) in used_variables.into_iter().enumerate() {
-                        if used {
-                            self.variables
-                                .get_unchecked(index)
-                                .add_activity(config::ACTIVITY_CONFLICT);
-                        }
+                    for index in buffer.variables_used() {
+                        self.variables
+                            .get_unchecked(index)
+                            .add_activity(config::ACTIVITY_CONFLICT);
                     }
                 }
             }
         }
-
-        (
-            resolved_clause,
-            ClauseSource::Resolution(resolution_trail),
-            asserted_literal.unwrap(),
-        )
     }
 
     pub fn display_core(&self) {
