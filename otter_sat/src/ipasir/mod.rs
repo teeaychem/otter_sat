2a42e177b426d6481756248a86d0437db1a4bb04
teeaychem <bsparkes@stanford.edu>
2025-02-06 12:43:06 +0000

    Further callback implementation
    - Callbacks have been moved to a separate file.
      Fields have been renamed to omit the 'ipasir_' prefix.
    
    - Callback for IPASIR2 additions has been added.
      The naming of the addition callbacks now follows the API.
      It is possible to set both callbacks, though these woud share length/data.
    
    - Version has been removed, as this increases the complexity of creating callbacks with very little beneift.

--- otter_sat/src/ipasir/mod.rs
+++ otter_sat/src/ipasir/mod.rs
@@ -35,6 +35,9 @@ use crate::{
     },
 };
 
+mod callbacks;
+pub use callbacks::IpasirCallbacks;
+
 pub mod ipasir_one;
 pub mod ipasir_two;
 
@@ -75,71 +78,6 @@ impl Default for ContextBundle {
             clause_buffer: Vec::default(),
             core_keys: Vec::default(),
             core_literals: HashSet::default(),
-        }
-    }
-}
-
-/// Information regarding the solve callback.
-pub struct IpasirCallbacks {
-    /// Version.
-    version: u8,
-
-    pub ipasir_terminate_callback: Option<extern "C" fn(data: *mut c_void) -> c_int>,
-    pub ipasir_terminate_data: *mut c_void,
-
-    pub ipasir_addition_callback: Option<extern "C" fn(data: *mut c_void, clause: *mut i32)>,
-    pub ipasir_addition_callback_length: u32,
-    pub ipasir_addition_data: *mut c_void,
-}
-
-impl IpasirCallbacks {
-    /// Calls the IPASIR addition callback, if defined.
-    ///
-    /// # Safety
-    /// The IPASIR API requires a pointer to the clause.
-    /// And, transmute is used to transmute a const pointer to a mutable pointer, if integer literals are used.
-    /// Safety can be restored by copying the clause, though this seems excessive.
-    /// Though, regardless of this, the method calls an external C function.
-    #[allow(clippy::useless_conversion)]
-    pub unsafe fn call_ipasir_addition_callback(&self, clause: &CClause) {
-        if let Some(addition_callback) = self.ipasir_addition_callback {
-            if clause.size() <= self.ipasir_addition_callback_length as usize {
-                if cfg!(feature = "boolean") {
-                    let mut int_clause: IntClause =
-                        clause.literals().map(|literal| literal.into()).collect();
-                    let callback_ptr: *mut i32 = int_clause.as_mut_ptr();
-
-                    addition_callback(self.ipasir_addition_data, callback_ptr);
-                } else {
-                    let clause_ptr: *const i32 = clause.as_ptr();
-                    let callback_ptr: *mut i32 = unsafe { std::mem::transmute(clause_ptr) };
-                    addition_callback(self.ipasir_addition_data, callback_ptr);
-                };
-            }
-        }
-    }
-
-    /// # Safety
-    /// Calls an external C function.
-    pub unsafe fn call_ipasir_terminate_callback(&self) -> i32 {
-        if let Some(terminate_callback) = self.ipasir_terminate_callback {
-            terminate_callback(self.ipasir_terminate_data)
-        } else {
-            1
-        }
-    }
-}
-
-impl Default for IpasirCallbacks {
-    fn default() -> Self {
-        Self {
-            version: 1,
-            ipasir_terminate_callback: None,
-            ipasir_terminate_data: std::ptr::dangling_mut(),
-
-            ipasir_addition_callback: None,
-            ipasir_addition_callback_length: 0,
-            ipasir_addition_data: std::ptr::dangling_mut(),
         }
     }
 }
